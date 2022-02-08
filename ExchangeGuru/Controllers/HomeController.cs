cc0e5282753ecce588b77fe08aeb1350e5dd4a85
joseph.kelly.87 <kelly.joseph1987@gmail.com>
2022-02-08 03:42:59 -0600

    Add local time/NYSE time to Website header

--- ExchangeGuru/Controllers/HomeController.cs
+++ ExchangeGuru/Controllers/HomeController.cs
@@ -27,6 +27,11 @@ namespace ExchangeGuru.Controllers
         {
             return View();
         }
+        
+        public IActionResult StockListings()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
